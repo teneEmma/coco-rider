547070bfeb7a3adacb38dbd1debe74c5d12cf862
TENE Emma <tenetampo@spreeloop.com>
2024-01-27 23:37:18 +0100

    phone_authentication_controller.dart updated

--- lib/pages/authentication/phone_authentication_controller.dart
+++ lib/pages/authentication/phone_authentication_controller.dart
@@ -1,4 +1,5 @@
 import 'package:coco_rider/common/widgets/coco_button.dart';
+import 'package:coco_rider/constants/internalization.dart';
 import 'package:coco_rider/pages/authentication/phone_authentication.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
@@ -6,6 +7,9 @@ import 'package:get/get.dart';
 class PhoneAuthenticationController extends GetxController {
   /// Text controller for the textField in [PhoneAuthentication].
   final phoneNumberTextController = TextEditingController();
+
+  /// Error text for the phone number text field.
+  final Rx<String?> phoneNumberTextFieldError = ''.obs;
 
   /// Button state for button to authenticate.
   final Rx<CocoButtonState> buttonState = CocoButtonState.initialState.obs;
@@ -16,11 +20,25 @@ class PhoneAuthenticationController extends GetxController {
 
   Future<void> authenticateUser() async {
     if (!_onValidate()) {
+      phoneNumberTextFieldError.value =
+          InternalizationKeys.errorFormatPhoneNumber.tr;
       return;
     }
+    phoneNumberTextFieldError.value = '';
   }
 
   String removeSpacesInNumber(String number) {
     return number.removeAllWhitespace;
+  }
+
+  void onPhoneNumberTextChanged() {
+    if (phoneNumberTextFieldError.value?.isNotEmpty == true) {
+      phoneNumberTextFieldError.value = '';
+    }
+    final digitsOnlyRegex = RegExp(r'^[0-9]+$');
+    if (!digitsOnlyRegex.hasMatch(phoneNumberTextController.text)) {
+      phoneNumberTextFieldError.value =
+          InternalizationKeys.errorNonDigitInNumber.tr;
+    }
   }
 }
