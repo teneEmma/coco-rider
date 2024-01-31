d4f5682e8667bae43f0b1c0cffa36189e9c80e42
Tene Tampo Emmanuel <103233184+teneEmma@users.noreply.github.com>
2024-01-31 23:52:42 +0100

    Internalization for otp code verification 2 (#31)
    * authentication response refactoring
    
    * internalization for otp verification page

--- lib/constants/internalization.dart
+++ lib/constants/internalization.dart
@@ -12,6 +12,9 @@ class CocoInternalization extends Translations {
               'Should only contain digits',
           InternalizationKeys.errorFormatPhoneNumber:
               'Wrong phone number format',
+          InternalizationKeys.errorAutoRetrievalTimeOut:
+              'Impossible de récupérer automatiquement le code.',
+          InternalizationKeys.errorOccurred: "Une erreur c'est produite.",
           InternalizationKeys.successText: 'Successful',
           InternalizationKeys.errorText: 'Error',
         },
@@ -24,6 +27,9 @@ class CocoInternalization extends Translations {
               'Doit seulement contenir des chiffres',
           InternalizationKeys.errorFormatPhoneNumber:
               'Le format du numero de téléphone est incorrect',
+          InternalizationKeys.errorAutoRetrievalTimeOut:
+              "Couldn't automatically retrieve the code.",
+          InternalizationKeys.errorOccurred: 'An error occurred.',
           InternalizationKeys.successText: 'Réussi',
           InternalizationKeys.errorText: 'Erreur',
         }
@@ -34,13 +40,15 @@ class InternalizationKeys {
   static const enterPhoneNumberText = 'enterPhoneNumberText';
   static const enterOTPCodeText = 'enterOTPCodeText';
   static const nextText = 'nextText';
-  static const prefixForPhoneNumberTextField = '+237';
+  static const countryCodeForPhoneNumberTextField = '+237';
   static const defaultTestPhoneNumber = '679001122';
   static const defaultTestVerificationId = 'ABC123XYZ';
   static const hintForPhoneNumberTextField = '000 000 000';
   static const hintForOTPDigitTextField = '0';
   static const errorNonDigitInNumber = 'errorNonDigitInNumber';
   static const errorFormatPhoneNumber = 'errorFormatPhoneNumber';
+  static const errorAutoRetrievalTimeOut = 'errorAutoRetrievalTimeOut';
+  static const errorOccurred = 'errorOccurred';
   static const successText = 'successText';
   static const errorText = 'errorText';
 }
