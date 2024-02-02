8d6ce76fd62568a64fb2e0ecc152a9072a1e1691
Tene Tampo Emmanuel <103233184+teneEmma@users.noreply.github.com>
2024-02-02 23:50:17 +0100

    created app constant file (#37)

---
+++ lib/constants/coco_app_constants.dart
@@ -0,0 +1,7 @@
+class CocoAppConstants {
+  static const int smallScreenWidth = 700;
+  static const int mediumScreenWidth = 1000;
+  static const double defaultSmallScreenAppBarHeight = 70;
+  static const double cocoTextFieldDefaultHeight = 70;
+  static const double cocoTextFieldDefaultWidth = 380;
+}
