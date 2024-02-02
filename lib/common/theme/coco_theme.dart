0745a1dbcb90aa2fb76cc388bdd742e86c6ea6e1
TENE Emma <tenetampo@spreeloop.com>
2024-02-02 23:52:21 +0100

    navigation rail theme for both day and night mode

--- lib/common/theme/coco_theme.dart
+++ lib/common/theme/coco_theme.dart
@@ -29,6 +29,25 @@ class CocoTheme {
         color: CocoColors.keyBlack.withAlpha(80),
       ),
     ),
+    navigationRailTheme: NavigationRailThemeData(
+      backgroundColor: CocoColors.keyWhite.withAlpha(150),
+      indicatorColor: CocoColors.keyPrimary.withAlpha(150),
+      useIndicator: true,
+      labelType: NavigationRailLabelType.all,
+      unselectedLabelTextStyle: const TextStyle(
+        color: CocoColors.keyGrey,
+        fontSize: 14,
+      ),
+      selectedIconTheme: const IconThemeData(
+        color: CocoColors.keyBlack,
+      ),
+      selectedLabelTextStyle: const TextStyle(color: CocoColors.keyBlack),
+    ),
+    appBarTheme: const AppBarTheme(
+      color: CocoColors.keyWhite,
+      toolbarHeight: CocoAppConstants.defaultSmallScreenAppBarHeight,
+      elevation: 0,
+    ),
     textSelectionTheme: TextSelectionThemeData(
       cursorColor: CocoColors.keyPrimary,
       selectionColor: CocoColors.keyPrimary.withAlpha(75),
@@ -77,6 +96,23 @@ class CocoTheme {
         letterSpacing: 2,
         color: CocoColors.keyWhite.withAlpha(150),
       ),
+    ),
+    navigationRailTheme: NavigationRailThemeData(
+      backgroundColor: CocoColors.keyBlack,
+      indicatorColor: CocoColors.keyPrimary.withAlpha(150),
+      useIndicator: true,
+      labelType: NavigationRailLabelType.all,
+      unselectedLabelTextStyle: const TextStyle(
+        color: CocoColors.keyGrey,
+        fontSize: 14,
+      ),
+      selectedIconTheme: const IconThemeData(
+        color: CocoColors.keyWhite,
+      ),
+      selectedLabelTextStyle: const TextStyle(color: CocoColors.keyBlack),
+    ),
+    appBarTheme: const AppBarTheme(
+      color: CocoColors.keyWhite,
     ),
     textSelectionTheme: TextSelectionThemeData(
       cursorColor: CocoColors.keyPrimary,
