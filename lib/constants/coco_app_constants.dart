d093d5725cc25a1c3dd78034d1cc3f7411eedb26
TENE Emma <tenetampo@spreeloop.com>
2024-02-03 00:01:03 +0100

    removed old coco_app_constants.dart

--- lib/constants/coco_app_constants.dart
+++
@@ -1,7 +0,0 @@
-class CocoAppConstants {
-  static const int smallScreenWidth = 700;
-  static const int mediumScreenWidth = 1000;
-  static const double defaultSmallScreenAppBarHeight = 70;
-  static const double cocoTextFieldDefaultHeight = 70;
-  static const double cocoTextFieldDefaultWidth = 380;
-}
